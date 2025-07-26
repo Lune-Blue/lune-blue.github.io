87ac6aaa92f384a779f58b5b84b254ab6f5ef3a1
Lune-Blue <tmdwns7546@yonsei.ac.kr>
2025-07-26 18:05:06 +0900

    cv new

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 gem "minimal-mistakes-jekyll"
 gem "jemoji"
+# Added at 2025-03-28 23:28:40 +0900 by lune:
+gem "webrick", "~> 1.9"
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,16 +1,1 @@
-var store = [{
-        "title": "Data Wrangling Project",
-        "excerpt":"H1 HeadingH2 HeadingH3 HeadingHere’s some basic text.And here’s some italicsHere’s some bold text.What about a link?Here’s a bulleted list: First item Second item Third itemHere’s a numbered list: First Second ThirdPython code block: import numpy as np def test_function(x, y): z = np.sum(x,y) return zR code block:library(tidyverse)df &lt;- read_csv(\"some_file.csv\")head(df)Here’s some...","categories": [],
-        "tags": ["data wrangling","data science","messy data"],
-        "url": "http://localhost:4000/perceptron/",
-        "teaser":null},{
-        "title": "Example Project Post-2",
-        "excerpt":"TitleHere’s some raw text.Here’s some MORE raw text…def hello_world():    print(\"hello world!\")print(hello_world())hello world!","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/example-post-2/",
-        "teaser":null},{
-        "title": "Example Project Post",
-        "excerpt":"TitleHere’s some raw text.Here’s some MORE raw text…def hello_world():    print(\"hello world!\")print(hello_world())hello world!","categories": [],
-        "tags": ["data wrangling","data science","messy data"],
-        "url": "http://localhost:4000/example-post/",
-        "teaser":null}]
+var store = []
